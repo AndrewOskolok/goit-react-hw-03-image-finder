d109878281882d70f1c5942a0843c116a8d4f2f7
Oscolok <oscolok7@gmail.com>
2020-07-21 19:55:44 +0300

    Updates

---
+++ precache-manifest.2fff25f03ee25b76cb172376eca24e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d47a764408c05e77d2d7076fae8bca3",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c9dcc709f1ad3e60ebdb",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.5a1501f6.chunk.css"
+  },
+  {
+    "revision": "d4a233e343af8208069e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.93d2d879.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.93d2d879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9dcc709f1ad3e60ebdb",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.246bde70.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
